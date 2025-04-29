4b58bdf4dfad44c019cddaa6332d7c3c372dd39a
CodeNinjaDan <dnlmuiru@gmail.com>
2025-04-29 14:12:43 +0300

    get practice

--- learn_rest_flask/files-cafe-api/main.py
+++ learn_rest_flask/files-cafe-api/main.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean
-
+from sqlalchemy import Integer, String, Boolean, select
+import random
 
 app = Flask(__name__)
 
@@ -30,13 +30,48 @@ class Cafe(db.Model):
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
 
-with app.app_context():
-    db.create_all()
+    def to_dict(self):
+        dictionary = {}
+
+        # Get column name and column value for each column and store them in a dict
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+# with app.app_context():
+#     db.create_all()
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+# HTTP GET - Read Record
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    all_cafes = db.session.execute(db.select(Cafe)).scalars().all()
+    random_cafe = random.choice(all_cafes)
+
+    return jsonify(cafe=random_cafe.to_dict())
+
+
+@app.route("/all", methods=["GET"])
+def get_all_cafes():
+    result = db.session.execute(db.select(Cafe)).scalars().all()
+    all_cafes = [cafes.to_dict() for cafes in result]
+
+
+    return jsonify(cafes=all_cafes)
+
+
+@app.route("/search/<string:location>", methods=["GET"])
+def get_cafes_by_location(location):
+    all_cafes = db.session.execute(db.select(Cafe).where(Cafe.location == location)).scalars().all()
+    if all_cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
+
 
 
 
